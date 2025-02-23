a9c35b1f888ee5f60b144c4df100569f69b7b307
Julia Perrin <juliaperrin@gwu.edu>
2025-02-23 16:38:45 -0500

    second file commit

--- Enigma.java
+++ Enigma.java
@@ -21,12 +21,46 @@ public class Enigma{
 
     public String decrypt(String message){        
         //TODO
+        int messageLength = message.length();
+        StringBuilder decryptedMessage = new StringBuilder();
+        for (int i = 0; i < messageLength; i++) {
+           char originalChar = message.charAt(i); //gets current character in the message (pre-decryption)
+           int outerIndex1 = rotors[2].indexOf(originalChar); //finds index of character in outer rotor
+           char midChar = rotors[1].charAt(outerIndex1); //find the character on the middle rotor aligned with the encrypted character on the outer
+           int outerIndex2 = rotors[2].indexOf(midChar); //finds the position on the outer rotor for the previously found character
+           char outerChar = rotors[2].charAt(outerIndex2); //finds the character at that positionally aligns with the previous on the outer rotor
+           int innerIndex = rotors[0].indexOf(outerChar); //finds the index of the previously found character
+           char decryptedChar = rotors[0].charAt(innerIndex); //finds the decrypted character by finding the aligning 
+           
+           decryptedMessage.append(decryptedChar); 
+           
+           rotate();
+        }
+        return decryptedMessage.toString();
     }
 
 
     
     public String encrypt(String message){
         //TODO
+        int messageLength = message.length();
+        StringBuilder encryptedMessage = new StringBuilder(); //build a string to store the encryption
+        for (int i = 0; i < messageLength; i++) {
+           char originalChar = message.charAt(i); //get current character in the message (Pre-encryption)
+           int innerIndex = rotors[0].indexOf(originalChar); //gets index of where character is in the inner rotor
+           char outerRotorChar = rotors[2].charAt(innerIndex); //finds the character at the same index in the outer rotor
+           int midIndex = rotors[1].indexOf(outerRotorChar); //gets index of character from outer rotor on the middle rotor
+           char midRotorChar = rotors[1].charAt(midIndex); //Finds the character at that index on the middle rotor 
+           int outerIndex = rotors[2].indexOf(midRotorChar); //finds index on outer rotor of character from middle rotor
+           char encryptedChar = rotors[2].charAt(outerIndex); //finds the final encrypted character at the outer rotor
+           
+           encryptedMessage.append(encryptedChar);
+           
+           rotate(); //call rotate method to rotate after every encrypted character
+
+        }
+        
+        return encryptedMessage.toString();
     }
 
     
--- Rotor.java
+++ Rotor.java
@@ -14,16 +14,23 @@ public class Rotor {
     
     public boolean rotate(){
         //TODO
+        char char27 = rotorValues.charAt(26); //Find last character
+        rotorValues = char27 + rotorValues.substring(0, 26); //remove last character, add to beginning, shift everything to the right
+        return rotorValues.charAt(0) == startChar; //Returns true if the rotor is back at starting position
                
     }
     
 
     public int indexOf(char c){
         //TODO
+        int index = rotorValues.indexOf(c);
+        return index;
     }
 
     public char charAt(int idx){
         //TODO
+        char foundHere = rotorValues.charAt(idx);
+        return foundHere;
     }
 }
     
